8148a3844ea47375b2e24322d62b24d901b19f3c
Ruthvik1811 <ruthvik1811@gmail.com>
2024-02-23 02:02:34 +0530

    Commit 1

---
+++ testgit/src/main/java/com/git/demo/testgit/Demogit.java
@@ -0,0 +1,10 @@
+package com.git.demo.testgit;
+
+public class Demogit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HEllo git");
+	}
+
+}
